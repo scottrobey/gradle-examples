530713f947285d2e2436200cab2688b064cef917
Scott Robey <scottrobey@gmail.com>
2018-11-26 14:33:27 -0600

    Swagger being served, need to download it as the last step

--- swag-gen/src/main/java/org/sample/swaggen/SwagGenerator.java
+++ swag-gen/src/main/java/org/sample/swaggen/SwagGenerator.java
@@ -3,6 +3,8 @@ package org.sample.swaggen;
 import java.net.URI;
 import java.util.UUID;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.HandlerList;
@@ -12,29 +14,36 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 
+
+/**
+ * Sets up an embedded Jetty server with Jersey 2 and Swagger using an JAX-RS Application configuration
+ * https://github.com/swagger-api/swagger-core/wiki/Swagger-Core-Jersey-2.X-Project-Setup-1.5#configure-and-initialize-swagger
+ */
 class SwagGenerator {
 
     public static void main(String[] args) {
+        final String uuid = UUID.randomUUID().toString();
 
         try {
             final Server server = new Server();
 
             final ServerConnector connector = new ServerConnector(server);
+            // pick a dynamic port
             connector.setPort(0);
-
             server.addConnector(connector);
 
             final ServletContextHandler root = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
 
-            final ResourceConfig config = new ResourceConfig();
-            config.packages("org.sample.swaggen");
+            ServletHolder restServlet = new ServletHolder(new ServletContainer(new ApplicationConfig(SwagGenerator.class.getPackage().getName())));
+            restServlet.setInitOrder(1);
+            root.addServlet(restServlet, "/*");
 
-            root.addServlet(new ServletHolder(new ServletContainer(config)), "/*");
-
-            String uuid = UUID.randomUUID().toString();
+            // setup shutdown handler
             ShutdownHandler shutdown = new ShutdownHandler(uuid, false, true);
-
             server.setHandler(new HandlerList(shutdown, root));
 
             sysout("Starting Jetty Server..." );
@@ -43,7 +52,10 @@ class SwagGenerator {
             final URI serverURI = server.getURI();
 
             sysout("Server started at: " + serverURI);
+            sysout("Swagger served at: " + serverURI + "swagger.json");
             sysout("To Shutdown: curl -X POST http://localhost:" + serverURI.getPort() + "/shutdown?token=" + uuid);
+
+            server.join();
 
         } catch (Throwable e) {
             syserr("Exception occurred", e);
@@ -59,5 +71,28 @@ class SwagGenerator {
     static void syserr(String msg, Throwable t) {
         syserr(msg);
         t.printStackTrace();
+    }
+
+    @ApplicationPath("/swagger")
+    static class ApplicationConfig extends ResourceConfig {
+        public ApplicationConfig(String ... pkgs) {
+            configureSwagger(pkgs);
+        }
+        public void configureSwagger(String ... pkgs) {
+            register(ApiListingResource.class);
+            register(SwaggerSerializers.class);
+
+            final String packageStr = String.join(",", pkgs);
+
+            final BeanConfig beanConfig = new BeanConfig();
+            beanConfig.setTitle("Test API");
+            beanConfig.setDescription("Test Rest API");
+            beanConfig.setVersion("0.0.1");
+            beanConfig.setBasePath("/*");
+            beanConfig.setResourcePackage(packageStr);
+            beanConfig.setScan(true);
+
+            packages(packageStr);
+        }
     }
 }
--- swag-gen/src/main/java/org/sample/swaggen/TheResource.java
+++ swag-gen/src/main/java/org/sample/swaggen/TheResource.java
@@ -3,10 +3,15 @@ package org.sample.swaggen;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @Path("/theresource")
+@Api(tags = "theresource")
 public class TheResource {
 
     @GET
+    @ApiOperation("my test endpoint")
     public String endpoint() {
         return "YoYoMa!";
     }
